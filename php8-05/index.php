3bd2dc34a0542c09db2a4b6cc4012469a71de2ed
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-04-30 13:03:16 -0300

    update

---
+++ php8-05/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP8</title>
+</head>
+<body>
+    <?php
+    $nome = "Ana";
+    $resultado = ($nome=="Ana")?"Professor":"Aluno";
+    ?>
+</body>
+</html>
---
+++ php8-06/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basic resourcers</title>
+</head>
+<body>
+    <h2>Date</h2>
+    <?php
+    echo date("d/m/y") . "<br>". // quando o y estiver em minúsculo só aparecerá somente os dois últimos números do anos
+         date("d/m/Y") . "<br>". // quando estiver em maiúsculo aparecerá o ano todo (ex: 2021) 
+         date("H:i") . "<br>". // hora e minuto ( i por causa que o m já tem em mês)
+         date("H:i:s") . "<br>"; // hora, minuto e segundo            
+    ?>
+
+    <h2>Redirect</h2>
+    <?php
+
+
+    ?>
+
+</body>
+</html>
